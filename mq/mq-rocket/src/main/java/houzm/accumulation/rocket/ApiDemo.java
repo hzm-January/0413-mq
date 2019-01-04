28b2d9cf7c03eec7588333e723ea112d4fdfcc82
hzm-January <hzm_dream@163.com>
2019-01-04 18:32:06 +0800

    first

---
+++ mq/mq-rocket/src/main/java/houzm/accumulation/rocket/ApiDemo.java
@@ -0,0 +1,77 @@
+package houzm.accumulation.rocket;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendCallback;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+/**
+ * author: hzm_dream@163.com
+ * date: 2019/1/2 14:39
+ * description: rocketMq
+ */
+public class ApiDemo {
+
+    public static void main(String[] args) throws Exception {
+
+        //同步消息发送
+//        syncProducer();
+        //异步消息发送
+        asyncProducer();
+    }
+
+
+    private static void syncProducer() throws Exception {
+        // Instantiate with a producer group name.
+        DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
+        // Specify name server addresses.
+        producer.setNamesrvAddr("47.101.152.55:9876");
+        // Launch the instance.
+        producer.start();
+        for (int i = 0; i < 100; i++) {
+            //Create a message instance, specifying topic, tag and message body.
+            Message msg = new Message("TopicTest" /* Topic */,
+                    "TagA" /* Tag */,
+                    ("Hello RocketMQ " +
+                            i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+            );
+            //Call send message to deliver message to one of brokers.
+            SendResult sendResult = producer.send(msg);
+            System.out.printf("%s%n", sendResult);
+        }
+    }
+
+    private static void asyncProducer() throws Exception {
+        //Instantiate with a producer group name.
+        DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
+        // Specify name server addresses.
+        producer.setNamesrvAddr("47.101.152.55:9876");
+        //Launch the instance.
+        producer.start();
+        producer.setRetryTimesWhenSendAsyncFailed(0);
+        for (int i = 0; i < 100; i++) {
+            final int index = i;
+            //Create a message instance, specifying topic, tag and message body.
+            Message msg = new Message("TopicTest",
+                    "TagA",
+                    "OrderID188",
+                    "Hello world".getBytes(RemotingHelper.DEFAULT_CHARSET));
+            producer.send(msg, new SendCallback() {
+                @Override
+                public void onSuccess(SendResult sendResult) {
+                    System.out.printf("%-10d OK %s %n", index,
+                            sendResult.getMsgId());
+                }
+                @Override
+                public void onException(Throwable e) {
+                    System.out.printf("%-10d Exception %s %n", index, e);
+                    e.printStackTrace();
+                }
+            });
+        }
+        //Shut down once the producer instance is not longer in use.
+        producer.shutdown();
+    }
+
+}
